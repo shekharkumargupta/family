212c153035f2f30c2c07d3df517c8e3ea82a8f19
Shekhar Kumar <shekharkumargupta@gmail.com>
2021-09-03 22:26:25 +0530

    started creating fields in PostForm

--- lib/view/PostForm.dart
+++ lib/view/PostForm.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
 
-class PostForm extends StatelessWidget{
+class PostForm extends StatefulWidget {
+
+  const PostForm ({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return PostFormState();
+  }
+}
+
+class PostFormState extends State<PostForm> {
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Post your feed'),),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.all(25.0),
+        child:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              createPostForm(context)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget createPostForm(BuildContext context){
+    return Container(
+      child: Form(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FloatingActionButton(
-              child: const Icon(Icons.arrow_back),
-              onPressed: (){
-                Navigator.pop(context);
-              },
+            TextFormField(
+              maxLines: 4,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+
+                ),
+                labelText: 'Express yourself here'
+              ),
             )
           ],
         ),
       ),
     );
   }
-
-
-
 }
