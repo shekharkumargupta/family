03e23094254eaf3516f123455249d4a0fae3867d
Shekhar Kumar <shekharkumargupta@gmail.com>
2021-09-04 23:09:48 +0530

    added camera and gallary on PostForm

--- lib/view/PostForm.dart
+++ lib/view/PostForm.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+import 'package:family/widgets/ImageSlider.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PostForm extends StatefulWidget {
 
@@ -13,6 +16,32 @@ class PostForm extends StatefulWidget {
 class PostFormState extends State<PostForm> {
 
   final _formKey = GlobalKey<FormState>();
+  File imageFile;
+
+  Future getImageFromCamera() async {
+    PickedFile pickedFile = await ImagePicker().getImage(
+      source: ImageSource.camera,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
+    }
+  }
+  Future getImageFromGallary() async {
+    PickedFile pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +62,7 @@ class PostFormState extends State<PostForm> {
   }
 
   Widget createPostForm(BuildContext context){
+
     return Container(
       child: Form(
         child: Column(
@@ -45,6 +75,33 @@ class PostFormState extends State<PostForm> {
                 ),
                 labelText: 'Express yourself here'
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 200.0,
+                child: Center(
+                  child: imageFile == null
+                      ? Text("No Image is picked")
+                      : ImageSlider().createImageItem(imageFile.path),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: getImageFromCamera,
+                  tooltip: "pickImage",
+                  child: Icon(Icons.add_a_photo),
+                ),
+                FloatingActionButton(
+                  onPressed: getImageFromGallary,
+                  tooltip: "Pick Image",
+                  child: Icon(Icons.camera_alt),
+                )
+              ],
             )
           ],
         ),
---
+++ lib/widgets/TakePictureScreenWidget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class TakePictureScreenWidget extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    return TakePictureScreenState();
+  }
+}
+
+class TakePictureScreenState extends State<TakePictureScreenWidget>{
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
